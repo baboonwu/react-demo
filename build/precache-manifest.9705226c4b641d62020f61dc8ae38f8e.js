a3115a673bf33b6eab9ede6a21ee7f4278e58d4d
baboonwu <xinwh@yeah.net>
2019-09-23 13:31:02 +0800

    demo

---
+++ build/precache-manifest.9705226c4b641d62020f61dc8ae38f8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a4cbc1e1090e87fa98a023633e632b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa3b190baec721156605",
+    "url": "/static/css/2.fa4a79a6.chunk.css"
+  },
+  {
+    "revision": "1daacb4b66905aae0e0e",
+    "url": "/static/css/main.87d24499.chunk.css"
+  },
+  {
+    "revision": "fa3b190baec721156605",
+    "url": "/static/js/2.953e12d3.chunk.js"
+  },
+  {
+    "revision": "1daacb4b66905aae0e0e",
+    "url": "/static/js/main.d67cfc08.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
